d719e48805adad76dbcafee390c1e6f94d83ba85
YuChia <h42279@gmail.com>
2021-05-23 22:04:30 +0800

    Init

---
+++ TextSplit/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TextSplit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
