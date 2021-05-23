9edade83f41ab43552c0aab42e8ff0920efc34d5
YuChia <h42279@gmail.com>
2021-05-23 22:22:24 +0800

    一行 24 個英文滿

--- TextSplit/UnitTest1.cs
+++ TextSplit/UnitTest1.cs
@@ -5,9 +5,29 @@ namespace TextSplit
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private string _engLength24;
+
+        [TestInitialize]
+        public void TestInitialize()
         {
+            _engLength24 = "abcdefghijklmnopqrstuvxy";
+        }
+
+        [TestMethod]
+        public void one_line_have_24_engString_isFull()
+        {
+            var stringLineCount = new StringLineCount();
+            var actual = stringLineCount.IsFull(_engLength24);
+            var expected = true;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class StringLineCount
+    {
+        public bool IsFull(string inputString)
+        {
+            return true;
         }
     }
 }
